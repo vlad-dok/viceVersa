9d9ab7544e58e3118c98488dab40751f3a39605c
Владислав Василюк <vladislav@MacBook-Pro-Vladislav.local>
2021-03-07 16:38:12 +0300

    Add words number

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,22 @@ def home(request):
 def reverse(request):
 	text_ru = request.GET['message']
 	reversed_text = text_ru[::-1]
-	return render (request,'reverse.html',{'message':text_ru, 
-		'reversedtext':reversed_text})
+	words = text_ru.split()
+	number_of_words = len(words)
+	return render (request,'reverse.html', {'message':text_ru, 
+		'reversedtext':reversed_text,'number_of_words':number_of_words})
+
+# def quantity(request):
+# 	words_text = print(len(text_ru.split())
+# 	# print(len(set(text_ru.split())))
+
+
+
+
+
+# 	words = set()
+# 	for _ in range(int(input())):
+#    		words.update(input().split())
+# 	# words_text = (len(words))
+# 	# return render (request,'reverse.html',{'message':text_ru, 
+# 	# 	})
